efabadd05f4ed501574b2830f060300adf8de333
naveedAhmed007 <naveed.ahmed007@ymail.com>
2023-04-27 11:50:51 +0500

    faceDEtection
    React Native camera vision
    react-native-face-detection
    
    App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,73 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
+import { Camera, useCameraDevices } from 'react-native-vision-camera';
+import FaceDetection, {
+  FaceDetectorContourMode,
+  FaceDetectorLandmarkMode,
+} from 'react-native-face-detection';
 const App = () => {
+  //REf Variables
+  const camera = useRef<Camera>(null);
+
+  //Hooks
+  const [image, setImage] = useState<string | undefined>();
+
+  //Camera Vars
+  const devices = useCameraDevices('wide-angle-camera');
+  const device = devices.front;
+  const options1 = {
+    landmarkMode: FaceDetectorLandmarkMode.ALL,
+    contourMode: FaceDetectorContourMode.ALL,
+    rotation: 0,
+    cameraOrientation: 'landscapeRight',
+  };
+
+  const handleInterval = async () => {
+    if (camera.current !== null) {
+      try {
+        const buffer = await camera.current.takePhoto({});
+        const faces = await FaceDetection.processImage(buffer.path, options1);
+        if (faces.length > 0) {
+          alert(`Total No of faces are ${faces.length}`);
+        }
+      } catch (error) {
+        console.log('Error in handleInterval:', error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(handleInterval, 5000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  useEffect(() => {
+
+    const getPermissions = async () => {
+      try {
+        const newCameraPermission = await Camera.requestCameraPermission();
+        console.log('permission==============', newCameraPermission);
+      } catch (error) {
+        console.log('Error in getPermissions:', error);
+      }
+    };
+    getPermissions();
+  }, []);
+
+
+  if (device == null) return <ActivityIndicator />;
+
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <>
+      <Camera
+        style={StyleSheet.absoluteFill}
+        device={device}
+        ref={camera}
+        isActive={true}
+        photo={true}
+      />
+    </>
   )
 }
 
